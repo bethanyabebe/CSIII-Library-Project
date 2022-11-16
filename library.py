96389e14f35202db42927727601d31562ebe6cfc
Ehnd89 <83450624+Ehnd89@users.noreply.github.com>
2022-11-16 03:53:31 -0500

    remove_book function in library.py

--- library.py
+++ library.py
@@ -28,8 +28,12 @@ class Library:
 
     @staticmethod
     def remove_book(isbn):
-        # delete book from file based on ISBN
-        pass
+        with open("books.txt",  "r") as inp:
+            with open("books.txt", "w") as output:
+                for line in inp:
+                    if isbn.GetValue() not in line.strip("\n"):
+                        output.write(line)
+        os.replace('temp.txt', 'books.txt')
 
     @staticmethod
     def rent_book(isbn, name):
